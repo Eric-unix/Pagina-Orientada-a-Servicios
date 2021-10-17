6f8f0061a79eb53547454248161d594fd80e40d5
Eric-unix <erixlopez6@gmail.com>
2021-10-17 15:56:31 -0500

    avances

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('/','home')->name('home');
+Route::view('acerca-de','about')->name('about');
+
+Route::get('blog','Blogcontroller@index')->name('blog.index');
+Route::get('blog/{post:slug}','Blogcontroller@show')->name('blog.show');
+
+Route::view('contactos','contact')->name('contact');
