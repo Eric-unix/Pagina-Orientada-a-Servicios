a5ebecbe858866f9161816188dd97848194e6afb
Eric-unix <erixlopez6@gmail.com>
2021-10-20 21:26:17 -0500

    first commit

--- public/index.php
+++ public/index.php
@@ -1,60 +1,168 @@
-<?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&family=Josefin+Slab:ital,wght@0,300;0,400;0,600;0,700;1,300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Pagina Orientada a Servicios</title>
+</head>
+<body>
 
-define('LARAVEL_START', microtime(true));
+</style>
+</head>
+<body>
+<header>
+        <div class="contenedor">
+            <nav class="menu">
+                <a href="#" id="">Inicio</a>
+                <a href="#" id="btn-acerca-de">Quienes somos</a>
+                <a href="#" id="btn-ubicacion">Contacto</a>
+                <a href="#" id="btn-ubicacion">Saber mas</a>
+            </nav>
+    
+            <div class="textos">
+                <h1 class="nombre"> <span>Servicios a ofrecer</span></h1>
+                <h3>
+                </h3>
+            </div>
+        </div>
+    </header>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
+    <section class="main">
+        <section class="acerca-de" id="acerca-de">
+            <div class="contenedor">
+                <div class="foto">
+                <img src="../../img" alt="">
+                </div>
+                <article>
+                    <h3>Acerca de</h3>
+                    <p>En esta pagina se ofrecen diferentes tipos de servicios medico que se puedan requerir en una emergencia o en cualquier situacion.</p>
+                </article>
+            </div>
+        </section>
 
-require __DIR__.'/../vendor/autoload.php';
+        <section class="menu">
+            <div class="contenedor">
+                <h3 class="titulo" id="platillos">Descripcion</h3>
+                <div class="contenedor-menu">
+                    <div class="contenedor-menu2">
+                        <article>
+                            <p class="categoria"></p>
+                            <div class="platillo">
+                                <p class="nombre">Medico General</p>
+                                <p class="precio">El médico general es el profesional de la medicina que cuenta con los conocimientos y las destrezas necesarias para diagnosticar y resolver con tratamiento medico y con procedimientos sencillos la mayoría de los padecimientos que el ser humano sufre en su vida, desde niño hasta la vejez, con acciones frecuentemente realizadas en el consultorio del medico o en la casa del enfermo.</p>
+                            </div>
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+                            <div class="platillo">
+                                <p class="nombre">Medico Cirujano</p>
+                                <p class="precio">El Médico Cirujano es un profesional formado para ofrecer servicios de medicina general de alta calidad, capaz de atender integralmente, tanto desde el punto de vista individual, familiar o comunitario las necesidades de promoción, prevención, recuperación y/o rehabilitación de la salud.</p>
+                            </div>
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+                            <div class="platillo">
+                                <p class="nombre">Medico Pediatra</p>
+                                <p class="precio">Médico que tiene una formación especial para prevenir, diagnosticar y tratar enfermedades y lesiones en los niños. Los pediatras también ayudan a tratar otros problemas que afectan a los niños.</p>
+                            </div>
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+                            <div class="platillo">
+                                <p class="nombre">Enfermera</p>
+                                <p class="precio">La enfermería es la ciencia que se dedica al cuidado y atención de enfermos y heridos, así como a otras tareas de asistencia sanitarias, clínicas y a la promoción y prevención de la salud.</p>
+                            </div>
+                        </article>
+                        <article>
+                            <p class="categoria"></p>
+                            <div class="platillo">
+                                <p class="nombre">Medico Familiar</p>
+                                <p class="precio">Este especialista, proporciona cuidado y atención permanente e integral a las personas y sus familias, teniendo en cuenta los componentes psicológicos, biológicos, laborales, familiares y sociales de cada persona.</p>
+                            </div>
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+                            <div class="platillo">
+                                <p class="nombre">Shakerato</p>
+                                <p class="precio">$20</p>
+                                <p class="descripcion">Ingredietnes: Espresso, simple syrup, y hielos, luego 'Shaken'</p>
+                            </div>
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+                            <div class="platillo">
+                                <p class="nombre">Espresso Tonic</p>
+                                <p class="precio">$25</p>
+                                <p class="descripcion">Espresso, tonic, con un twist de naranja</p>
+                            </div>
 
-$response->send();
+                            <div class="platillo">
+                                <p class="nombre">Frappés</p>
+                                <p class="precio">$15</p>
+                                <p class="descripcion">Puede escoger entre los diferentes sabores: Cappuccino, Caramelo, Chai, Matcha</p>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
 
-$kernel->terminate($request, $response);
+        <section class="galeria" id="galeria">
+            <div class="foto">
+                <img src="img/1.jpg" alt="">
+            </div>
+            <div class="foto">
+                <img src="img/2.jpg" alt="">
+            </div>
+            <div class="foto">
+                <img src="img/3.jpg" alt="">
+            </div>
+            <div class="foto">
+                <img src="img/4.jpg" alt="">
+            </div>
+            <div class="foto">
+                <img src="img/5.jpg" alt="">
+            </div>
+            <div class="foto">
+                <img src="img/6.jpg" alt="">
+            </div>
+            <div class="foto">
+                <img src="img/7.jpg" alt="">
+            </div>
+            <div class="foto">
+                <img src="img/8.jpg" alt="">
+            </div>
+        </section>
+        
+
+        <section class="ubicacion" id="ubicacion">
+            <div class="contenedor">
+                <h3 class="titulo">Ubicacion</h3>
+                <div class="direccion">
+                    <p class="calle">Calle Insurgentes poniente #213 <br />Colonia Centro, CP 90500</p><br>
+                    <p class="telefono">(247) 132 08 83</p>
+                    <p class="correo">erixlopez6@gmail.com</p>
+                </div>
+
+                <div class="horarios">
+                    <h4>Horarios</h4>
+                    <p class="entre-semana">Lunes a Viernes <br> 8:00am - 9:00pm</p>
+                    <p class="fin-semana">Sabados y Domingos<br> 11:00am - 11:00pm</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="mapa">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30123.346823943077!2d-97.92577805376246!3d19.30764444368545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cffc84a59d6b89%3A0x53024f948d1f4a99!2sInsurgentes%20213%2C%20Centro%2C%2090501%20Huamantla%2C%20Tlax.!5e0!3m2!1ses-419!2smx!4v1624218355041!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </section>
+
+    </section>
+
+    <footer>
+        <div class="copyright">
+            <p>copyright Eric Barrera @ 2021</p>
+        </div>
+    </footer>
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/efectos.js"></script>
+    <script src="js/parallax.js"></script>
+</body>
+</html>
+</body>
+</html>
